4ff06d476fcbb6c0e3d9e1b980c8bc3585fec43f
maheshwaranv <mahes1287@gmail.com>
2020-12-14 14:06:39 +0530

    spinner feature was added

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const newQuoteButton = document.querySelector('#js-new-quote');
 newQuoteButton.addEventListener('click', getQuote);
 
 const endpoint = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random';
-
+const spinner = document.querySelector('#js-spinner');
 async function getQuote() {
+    spinner.classList.remove('hidden');
+    newQuoteButton.disabled = true;
 try {
     const response = await fetch(endpoint);
     if (!response.ok) {
@@ -16,6 +18,9 @@ try {
 } catch (err) {
   console.log(err);
   alert('Failed to fetch the quote')  
+} finally {
+    spinner.classList.add('hidden');
+    newQuoteButton.disabled = false;
 }
 }
 
